07ce720b1283889ccd2ab25f13b4dc7058c1566f
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-03-29 23:08:11 +0000

    Remove usage of Lombok and add manual builder

--- src/main/java/com/andrealaforgia/italianfiscalcode/Person.java
+++ src/main/java/com/andrealaforgia/italianfiscalcode/Person.java
@@ -1,8 +1,5 @@
 package com.andrealaforgia.italianfiscalcode;
 
-import lombok.Builder;
-import lombok.Value;
-
 import java.time.LocalDate;
 
 enum Gender {
@@ -10,12 +7,79 @@ enum Gender {
     Female
 }
 
-@Builder
-@Value
 public class Person {
-    String firstName;
-    String lastName;
-    LocalDate dateOfBirth;
-    String placeOfBirthCode;
-    Gender gender;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String placeOfBirthCode;
+    private Gender gender;
+
+    private Person(Builder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.dateOfBirth = builder.dateOfBirth;
+        this.placeOfBirthCode = builder.placeOfBirthCode;
+        this.gender = builder.gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getPlaceOfBirthCode() {
+        return placeOfBirthCode;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private LocalDate dateOfBirth;
+        private String placeOfBirthCode;
+        private Gender gender;
+
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder dateOfBirth(LocalDate dateOfBirth) {
+            this.dateOfBirth = dateOfBirth;
+            return this;
+        }
+
+        public Builder placeOfBirthCode(String placeOfBirthCode) {
+            this.placeOfBirthCode = placeOfBirthCode;
+            return this;
+        }
+
+        public Builder gender(Gender gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
 }
